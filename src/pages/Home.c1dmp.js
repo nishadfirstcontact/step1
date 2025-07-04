4fa0746ab4e39c319fd9f4cbd2bc3a83fd9da4a7
nishadfirstcontact <nishad.firstcontact@gmail.com>
2025-07-04 22:54:09 +0530

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,10 +1,34 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+import wixData from 'wix-data';
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    loadMentors();
 });
+
+function loadMentors() {
+    wixData.query('Mentors') // collection name
+        .find()
+        .then((results) => {
+            if (results.items.length > 0) {
+                let items = results.items;
+                $w("#repeater1").data = items;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+// When the repeater item is ready
+export function repeater1_itemReady($item, itemData, index) {
+    $item("#nameText").text = itemData.name; // connect to text inside the box
+    $item("#designationText").text = itemData.designation;
+    $item("#descriptionText").text = itemData.description;
+    $item("#bioText").text = itemData.bio;
+    $item("#availabilityText").text = itemData.availability;
+    $item("#whoShouldText").text = itemData['whoShouldReachOutToYou'];
+    $item("#mentorImage").src = itemData.image;
+    $item("#linkedinButton").link = itemData['linkedinURL'];
+    $item("#contactButton").link = itemData['contactURL'];
+}
