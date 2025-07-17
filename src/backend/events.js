4a89bde9c4b25967041a1c6cd75e1394dfbf1d2b
nishadfirstcontact <nishad.firstcontact@gmail.com>
2025-07-17 20:08:06 +0530

    Update events.js

--- src/backend/events.js
+++ src/backend/events.js
@@ -1,4 +1,5 @@
 import { setGuestlist } from 'backend/gatherAPICalls.jsw';
+import { fetch } from 'wix-fetch';
 
 export async function wixMembers_onMemberUpdated(event) {
     const member = event.entity;
@@ -11,4 +12,21 @@ export async function wixMembers_onMemberUpdated(event) {
         console.log("Approved user:", memberEmail);
         await setGuestlist(memberEmail, memberNickname);
     }
+}
+
+
+
+
+export function FullData_afterInsert(item, context) {
+    const endpoint = "https://firstcontactlgbt.wixsite.com/_functions/receiveStudioMember";
+    const apiKey = "my-super-secret-api-key-12345";
+
+    return fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": apiKey
+        },
+        body: JSON.stringify(item)
+    });
 }
